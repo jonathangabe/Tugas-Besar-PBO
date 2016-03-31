5fd213db4cafcb7b675eb63a8cf4f8b397d1d8f7
jonathangabe <panggabean_jonathan@yahoo.co.id>
2016-03-31 21:20:53 +0700

    Merge origin/master
    Conflicts:
    	README.md

--- LowonganPekerjaan/src/lowonganpekerjaan/LowonganPekerjaan.java
+++ LowonganPekerjaan/src/lowonganpekerjaan/LowonganPekerjaan.java
@@ -3,6 +3,8 @@ package lowonganpekerjaan;
 public class LowonganPekerjaan {
     public static void main(String[] args) {
         // TODO code application logic here
+        Aplikasi cobaApp = new Aplikasi();
+        cobaApp.mainMenu();
     }
     
 }
